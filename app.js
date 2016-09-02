bf8055715e4fcdb97119f41e8b3c71ce12181cf9
sebnun <sebnun@gmail.com>
2016-09-02 01:05:19 -0300

    bug fix

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const moment = require("moment");
 
 http.createServer(function(req, res) {
 
-    if (req.url === "/") {
+    if (req.url === "/" || req.url === "//") {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(`<p>See <a href="https://github.com/sebnun/timestamp">Timestamp</a> for more info.</p>`)
     } else {
-        
-        //req.url on my server is //something
+        //req.url on my server is "//something"
         const userDate = decodeURI(req.url.substring(2)); 
         //isNaN(num) true if not a number
         const parsedDate = isNaN(userDate) ? moment(userDate) : moment.unix(userDate);
